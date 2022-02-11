4cc8771fcf07e79934c1d539c6dcb61bc91b8d09
Manmellon <24492231+Manmellon@users.noreply.github.com>
2022-02-11 14:39:04 +0300

    Added general gameplay

---
+++ Assets/Scripts/Ground.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ground : MonoBehaviour
+{
+    [SerializeField] MovingObject _movingObject;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray.origin, ray.direction, out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Ground")))
+            {
+                //Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red);
+
+                if (hit.normal == Vector3.up)
+                {
+                    //Debug.Log("Its upper face" + hit.point);
+                    _movingObject.AddWayPoint(hit.point);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MovingObject.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingObject : MonoBehaviour
+{
+    private List<Vector3> _wayPoints;
+    private int _currentPointIndex;
+
+    [SerializeField] private float _movingSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentPointIndex = 0;
+
+        _wayPoints = new List<Vector3>();
+        AddWayPoint(transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_currentPointIndex < _wayPoints.Count - 1)
+        {
+            float currentDistance = Vector3.Distance(_wayPoints[_currentPointIndex], _wayPoints[_currentPointIndex + 1]);
+            float passedDistance = Vector3.Distance(_wayPoints[_currentPointIndex], transform.position);
+
+            float fraction = (passedDistance + _movingSpeed * Time.deltaTime) / currentDistance;
+
+            transform.position = Vector3.Lerp(_wayPoints[_currentPointIndex], _wayPoints[_currentPointIndex + 1], fraction);
+
+            if (fraction >= 1.0f)
+            {
+                _currentPointIndex += 1;
+            }
+        }
+    }
+    
+    public void AddWayPoint(Vector3 coords)
+    {
+        _wayPoints.Add(coords);
+    }
+
+    public void SetMovingSpeed(float newSpeed)
+    {
+        _movingSpeed = newSpeed;
+    }
+}
